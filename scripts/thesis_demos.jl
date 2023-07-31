4fa9c49a7ae4e31980d2d67a5ca893ecc657f5b2
Dylan Asmar <asmar@stanford.edu>
2023-07-31 00:52:41 -0700

    demo param updates

--- scripts/thesis_demos.jl
+++ scripts/thesis_demos.jl
@@ -2,107 +2,129 @@ include("full_demo.jl")
 
 ## LOAD LDRAW FILE
 
-# project_name                = "colored_8x8.ldr"
-# model_scale                 = 0.01
-# num_robots                  = 24
-# object_vtx_range            = (-10:10,-10:10, 0:1)
-# home_vtx_range              = (-10:-2, -10:-7, 0:0)
-# max_steps                   = 3000
-# robot_scale                 = model_scale * 0.9
-
-# project_name              = "quad_nested.mpd"
-# model_scale               = 0.004
-# num_robots                = 50
-# robot_scale               = model_scale
-# object_vtx_range          = (-10:10,-10:10, 0:2)
-# max_steps                 = 3000
-# home_vtx_range            = (-10:10,-10:10, 0:0)
-# staging_buffer_factor     = 1.5
-# build_step_buffer_factor  = 0.55
-
-# project_name                = "tractor.mpd"
+# project_name = "colored_8x8.ldr" #* 33 x 1
 # model_scale                 = 0.008
 # robot_scale                 = model_scale * 0.7
-# num_robots                  = 12
+# num_robots                  = 24
 # max_steps                   = 3000
-# object_vtx_range            = (-10:10,-10:10, 0:1)
-# home_vtx_range              = (-10:-4, -10:-8, 0:0)
-# staging_buffer_factor       = 1.5
-# build_step_buffer_factor    = 1.5
+# staging_buffer_factor       = 1.2
+# build_step_buffer_factor    = 0.5
 
-# project_name = "30051-1 - X-wing Fighter - Mini.mpd"
-# model_scale               = 0.007
+# project_name = "quad_nested.mpd" #* 85 x 21
+# model_scale               = 0.008
 # robot_scale               = model_scale * 0.7
-# num_robots                = 30
-# max_steps                 = 8000
-# object_vtx_range          = (-12:12, -12:12, 0:0)
-# home_vtx_range            = (-15:-5, -15:-10, 0:0)
-# staging_buffer_factor     = 1.5
-# build_step_buffer_factor  = 1.5
-
-# project_name = "ATTEWalker.mpd"
-# model_scale                 = 0.003
-# robot_scale                 = model_scale
-# num_robots                  = 36
-# max_steps                   = 8000
-# object_vtx_range            = (-10:10,-10:10, 0:1)
-# home_vtx_range              = (-10:-1, -10:-6, 0:0)
-# staging_buffer_factor       = 1.5
-# build_step_buffer_factor    = 1.5
-
-# project_name = "7140-1 - X-wing Fighter--mod.mpd"
-# model_scale               = 0.0028
-# robot_scale               = model_scale
-# num_robots                = 100 #? Maybe less?
-# max_steps                 = 15000
-# object_vtx_range          = (-14:0.5:14, -14:0.5:14, 0:0)
-# home_vtx_range            = (-25:0, -25:-20, 0:0)
-# staging_buffer_factor     = 1.5
+# num_robots                = 50
+# max_steps                 = 3000
+# staging_buffer_factor     = 1.2
 # build_step_buffer_factor  = 0.5
 
-project_name = "6080 - Kings Castle.mpd"
-model_scale               = 0.0025
-num_robots                = 75
-robot_scale               = model_scale
-max_steps                 = 40000
-object_vtx_range          = (-30:0.5:30, -30:0.5:30, 0:0)
-home_vtx_range            = (-35:-20, -35:-20, 0:0)
-staging_buffer_factor     = 1.5
-build_step_buffer_factor  = 0.5
-
-# project_name = "StarDestroyer.mpd"
-# model_scale               = 0.002
-# num_robots                = 100
-# robot_scale               = model_scale
+# project_name = "heavily_nested.mpd" #* 1757 x 508
+# model_scale               = 0.008
+# num_robots                = 50
+# robot_scale               = model_scale * 0.7
 # max_steps                 = 40000
-# object_vtx_range          = (-16:0.5:16, -16:0.5:16, 0:0)
-# home_vtx_range            = (-25:0, -25:-20, 0:0)
-# staging_buffer_factor     = 1.5
+# staging_buffer_factor     = 1.2
 # build_step_buffer_factor  = 0.5
 
-# project_name = "heavily_nestes.mpd"
+# project_name = "tractor.mpd" #* 20 x 8
+# model_scale                 = 0.008
+# robot_scale                 = model_scale * 0.7
+# num_robots                  = 10
+# max_steps                   = 5000
+# staging_buffer_factor       = 1.2
+# build_step_buffer_factor    = 0.5
+
+# project_name = "8028-1 - TIE Fighter - Mini.mpd" #* 44 x 4
+# model_scale               = 0.008
+# num_robots                = 15
+# robot_scale               = model_scale * 0.7
+# max_steps                 = 40000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "4494-1 - Imperial Shuttle - Mini.mpd" #* 84 x 5
+# model_scale               = 0.008 # 0.0028
+# robot_scale               = model_scale * 0.7
+# num_robots                = 15
+# max_steps                 = 15000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "30051-1 - X-wing Fighter - Mini.mpd" #* 61 x 12
+# model_scale               = 0.008
+# robot_scale               = model_scale * 0.7
+# num_robots                = 20
+# max_steps                 = 8000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "X-wing--Tie Mini.mpd" #* 105 x 17
+# model_scale               = 0.008
+# num_robots                = 20
+# robot_scale               = model_scale * 0.7
+# max_steps                 = 40000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "20009-1 - AT-TE Walker - Mini.mpd" #* 100 x 22
+# model_scale                 = 0.008
+# robot_scale                 = model_scale * 0.7
+# num_robots                  = 35
+# max_steps                   = 8000
+# staging_buffer_factor       = 1.2
+# build_step_buffer_factor    = 0.5
+
+# project_name = "7140-1 - X-wing Fighter.mpd" #* 309 x 28
+# model_scale               = 0.008 # 0.0028
+# robot_scale               = model_scale * 0.7
+# num_robots                = 50
+# max_steps                 = 15000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "3181 - Passenger Plane.mpd" #* 326 x 28
 # model_scale               = 0.004
 # num_robots                = 50
-# robot_scale               = model_scale
-# object_vtx_range          = (-350:0.5:35,-35:0.5:35, 0:2)
+# robot_scale               = model_scale * 0.7
 # max_steps                 = 40000
-# home_vtx_range            = (-60:0.5:0,-60:0.5:0, 0:0)
-# staging_buffer_factor     = 1.5
-# build_step_buffer_factor  = 0.55
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
 
-# project_name = "Saturn.mpd"
+# project_name = "8099-1 - Midi-Scale Imperial Star Destroyer.mpd" #* 418 x 11
+# model_scale               = 0.004
+# num_robots                = 75
+# robot_scale               = model_scale * 0.7
+# max_steps                 = 40000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "6080 - Kings Castle.mpd" #* 761 x 70
+# model_scale               = 0.004
+# num_robots                = 125
+# robot_scale               = model_scale * 0.7
+# max_steps                 = 40000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "75054-1 - AT-AT.mpd" #* 1105 x 2
+# model_scale               = 0.004
+# num_robots                = 100
+# robot_scale               = model_scale * 0.7
+# max_steps                 = 40000
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
+
+# project_name = "21309-1 - NASA Apollo Saturn V.mpd" #* 1845 x 306
 # model_scale               = 0.0015
-# num_robots                = 50 #100
-# robot_scale               = model_scale
+# num_robots                = 200
+# robot_scale               = model_scale * 0.7
 # max_steps                 = 100000
-# object_vtx_range          = (-30:0.5:30, -30:0.5:30, 0:1)
-# home_vtx_range            = (-50:0.5:0,-50:0, 0:0)
-# staging_buffer_factor     = 1.5
-# build_step_buffer_factor  = 1.5
+# staging_buffer_factor     = 1.2
+# build_step_buffer_factor  = 0.5
 
 visualize_processing         = false
-visualize_animation_at_end   = false
-save_animation_along_the_way = false
+visualize_animation_at_end   = true
+save_animation_along_the_way = true
 save_animation_at_end        = true
 anim_steps                   = true
 anim_active_areas            = true
@@ -112,7 +134,12 @@ write_results                = true
 overwrite_results            = true
 seed                         = 1
 
-max_num_iters_no_progress    = 1500
+num_object_layers            = 1
+max_num_iters_no_progress    = 10000
+
+sim_batch_size               = 50
+process_updates_interval     = 25
+save_anim_interval           = 100
 
 using Gurobi
 
@@ -121,8 +148,7 @@ env, STATS = run_lego_demo(;
     model_scale                  = model_scale,
     num_robots                   = num_robots,
     robot_scale                  = robot_scale,
-    object_vtx_range             = object_vtx_range,
-    home_vtx_range               = home_vtx_range,
+    num_object_layers            = num_object_layers,
     max_steps                    = max_steps,
     assignment_mode              = assignment_mode,
     rvo_flag                     = rvo_flag,
@@ -136,5 +162,8 @@ env, STATS = run_lego_demo(;
     overwrite_results            = overwrite_results,
     max_num_iters_no_progress    = max_num_iters_no_progress,
     default_milp_optimizer       = Gurobi.Optimizer,
+    sim_batch_size               = sim_batch_size,
+    process_updates_interval     = process_updates_interval,
+    save_anim_interval           = save_anim_interval,
     seed                         = seed
 );
