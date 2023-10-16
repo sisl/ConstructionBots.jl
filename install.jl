699dfda81ff28cc22de1a946022f6abedeb33560
dylan-asmar <asmar@stanford.edu>
2023-10-16 14:39:14 -0700

    ssh to https change

--- install.jl
+++ install.jl
@@ -3,11 +3,11 @@ import Pkg: Pkg, PackageSpec
 Pkg.activate(".")
 
 packages = [
-    PackageSpec(url="git@github.com:sisl/CRCBS.jl.git"),
-    PackageSpec(url="git@github.com:sisl/GraphUtils.jl.git"),
-    PackageSpec(url="git@github.com:sisl/TaskGraphs.jl.git"),
-    PackageSpec(url="git@github.com:sisl/HierarchicalGeometry.jl.git"),
-    PackageSpec(url="git@github.com:sisl/LDrawParser.jl.git")
+    PackageSpec(url="https://github.com/sisl/CRCBS.jl.git"),
+    PackageSpec(url="https://github.com/sisl/GraphUtils.jl.git"),
+    PackageSpec(url="https://github.com/sisl/TaskGraphs.jl.git"),
+    PackageSpec(url="https://github.com/sisl/HierarchicalGeometry.jl.git"),
+    PackageSpec(url="https://github.com/sisl/LDrawParser.jl.git")
 ]
 Pkg.add(packages)
 
