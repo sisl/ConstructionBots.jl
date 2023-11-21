9ebefeef48f9c53752e6d3863b033552aec74bb5
Dylan Asmar <asmar@stanford.edu>
2023-11-21 10:55:51 -0700

    updated demo default

--- scripts/demos.jl
+++ scripts/demos.jl
@@ -1,21 +1,21 @@
 using ConstructionBots
 
-#                                    # parts      # assemblies      # estimated time (rvo+)
-#     1 => :colored_8x8                 33               1                   4 sec
-#     2 => :quad_nested                 85              21                  26 sec
-#     3 => :heavily_nested              1757           508                  62 min
-#     4 => :tractor                     20               8                   2 sec
-#     5 => :tie_fighter                 44               4                   7 sec
-#     6 => :x_wing_mini                 61              12                   9 sec
-#     7 => :imperial_shuttle            84               5                  13 sec
-#     8 => :x_wing_tie_mini             105             17                  26 sec
-#     9 => :at_te_walker                100             22                  23 sec
-#     10 => :x_wing                     309             28                   3 min
-#     11 => :passenger_plane            326             28                   4 min
-#     12 => :imperial_star_destroyer    418             11                   5 min
-#     13 => :kings_castle               761             70                  21 min
-#     14 => :at_at                      1105             2                  ??
-#     15 => :saturn_v                   1845           306                 163 min
+#                                    # parts      # assemblies
+#     1 => :colored_8x8                 33               1
+#     2 => :quad_nested                 85              21
+#     3 => :heavily_nested              1757           508
+#     4 => :tractor                     20               8
+#     5 => :tie_fighter                 44               4
+#     6 => :x_wing_mini                 61              12
+#     7 => :imperial_shuttle            84               5
+#     8 => :x_wing_tie_mini             105             17
+#     9 => :at_te_walker                100             22
+#     10 => :x_wing                     309             28
+#     11 => :passenger_plane            326             28
+#     12 => :imperial_star_destroyer    418             11
+#     13 => :kings_castle               761             70
+#     14 => :at_at                      1105             2
+#     15 => :saturn_v                   1845           306
 
 project_params = get_project_params(4)
 
@@ -23,19 +23,20 @@ project_params = get_project_params(4)
 open_animation_at_end = true
 save_animation_along_the_way = false
 save_animation_at_end = false
-anim_active_agents = true
-anim_active_areas = true
+anim_active_agents = true # green circles around active agents (robots and transport units)
+anim_active_areas = true # purple circles around active assembly areas
+
 update_anim_at_every_step = true
+save_anim_interval = 100
+process_updates_interval = 100
+block_save_anim = false
 
 tangent_bug_flag = true
 rvo_flag = true
 dispersion_flag = true
-assignment_mode = :greedy
-# assignment_mode = :milp
-# assignment_mode = :milp_w_greedy_warm_start
-
-write_results = false
-overwrite_results = false
+assignment_mode = :greedy # :milp :milp_w_greedy_warm_start
+milp_optimizer = :highs # :gurobi :highs
+optimizer_time_limit = 60
 
 
 env, stats = run_lego_demo(;
@@ -44,8 +45,8 @@ env, stats = run_lego_demo(;
     model_scale=project_params[:model_scale],
     num_robots=project_params[:num_robots],
     assignment_mode=assignment_mode,
-    milp_optimizer=:highs, # :gurobi :highs
-    optimizer_time_limit=300,
+    milp_optimizer=milp_optimizer,
+    optimizer_time_limit=optimizer_time_limit,
     rvo_flag=rvo_flag,
     tangent_bug_flag=tangent_bug_flag,
     dispersion_flag=dispersion_flag,
@@ -55,9 +56,11 @@ env, stats = run_lego_demo(;
     anim_active_agents=anim_active_agents,
     anim_active_areas=anim_active_areas,
     update_anim_at_every_step=update_anim_at_every_step,
-    save_anim_interval=100,
-    write_results=write_results,
-    overwrite_results=overwrite_results,
+    save_anim_interval=save_anim_interval,
+    process_updates_interval=process_updates_interval,
+    block_save_anim=block_save_anim,
+    write_results=false,
+    overwrite_results=false,
     look_for_previous_milp_solution=false,
     save_milp_solution=false,
     previous_found_optimizer_time=30,
