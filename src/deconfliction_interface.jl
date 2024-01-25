9e2337cb51bbd4866e52ec506942f882f728e671
Tasha Kim <tashakim@google.com>
2024-01-25 19:07:42 +0000

    Implement new deconfliction interface to integrate with demo
    Followup: direct calls to RVO interface should be replaced with calls
    to this new interface that takes in a parameter specifying the
    deconfliction algorithm type.
    
    Test: Ran all tests
    Bug: #38

---
+++ src/deconfliction_interface.jl
@@ -0,0 +1,44 @@
+# TODO(tashakim): Import rvo_interface and other deconfliction 
+# algorithm implementations
+
+# TODO(tashakim): Define DeconflictionType enum to pass in
+
+# Update the simulation environment by specifying new agent properties.
+function update_simulation_environment(deconfliction_type)
+    if deconfliction_type == "rvo"
+        return rvo_set_new_sim!()
+    else
+        println("No simulation update required: $deconfliction_type")
+    end
+end
+
+# Add agents to simulation based on the deconfliction algorithm used.
+function add_agents_to_simulation(scene_tree, deconfliction_type)
+    if deconfliction_type == "rvo"
+        return rvo_add_agents!(scene_tree)
+    else
+        println("No new agents to add: $deconfliction_type")
+    end
+end
+
+# Adjust the priority of agents to manage their interactions and avoid
+# potential gridlocks. 
+function set_agent_priority(env::PlannerEnv, node)
+    if deconfliction_type == "rvo"
+        return set_rvo_priority!(env, node)
+    else
+        println("No agent priority to update: $deconfliction_type")
+    end
+end
+
+# Return the maximum speed of a node based on its type and volume.
+function get_vmax(node, deconfliction_type)
+    if deconfliction_type == "rvo"
+        return get_rvo_max_speed(node)  # RVOInterface.get_rvo_max_speed(node)
+    else
+        # Handle other deconfliction types or throw an error
+        error("Unsupported deconfliction type: $deconfliction_type")
+    end
+end
+
+# TODO(tashakim): Add additional methods for minimal functionality, as appropriate
